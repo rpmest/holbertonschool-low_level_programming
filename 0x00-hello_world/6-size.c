a8cc733ac38ef8c70db5cf52701704d3cc7da553
Ryan Moran <rpmest@gmail.com>
2020-09-16 16:29:25 +0000

    Wrote C program 6-size.c that prints the size in bytes of various data types

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the size of variable types
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	printf("Size of a char: %i byte(s)\n", (int) sizeof(char));
+	printf("Size of an int: %i byte(s)\n", (int) sizeof(int));
+	printf("Size of a long: %i byte(s)\n", (int) sizeof(long));
+	printf("Size of a long long: %i byte(s)\n", (int) sizeof(long long));
+	printf("Size of a float: %i byte(s)\n", (int) sizeof(float));
+
+	return (0);
+}
