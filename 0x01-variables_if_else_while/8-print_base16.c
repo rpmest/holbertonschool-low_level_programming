e69f2ea47bcf4e5c973772976c5bba01c93d3790
Ryan Moran <rpmest@gmail.com>
2020-09-17 17:43:38 +0000

    Wrote program 8-print_base16.c, which prints the numbers of base 16 in lowercase, followed by a new line

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the numbers of base 16 in lowercase
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 48; num < 64; num++)
+	{
+		if (num > 57)
+		{
+			putchar(num + 7);
+		}
+		else
+		{
+			putchar(num);
+		}
+	}
+	/*Prints newline*/
+	putchar(10);
+
+	return (0);
+}
